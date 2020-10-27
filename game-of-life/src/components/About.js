875479c7edd3c46fa609ab6da27cd1f3df95ebcd
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-10-26 21:02:09 -0500

    updated about and rules

--- game-of-life/src/components/About.js
+++ game-of-life/src/components/About.js
@@ -4,6 +4,13 @@ function About() {
     return (
         <>
             <p>In 1940, John Conway, a British mathematician, invented the cellular automaton called Game of Life. Game of Life is a zero-player game, where the evolution of cells is determined by it's initial state. Along with this evolution of cells, this requires the player to have no further input.</p>
+            <p>In theory, the Game of Life has the power of a <a className="active-link" href="https://en.wikipedia.org/wiki/Turing_machine" target="_blank" rel="noopener noreferrer">universal Turing machine</a> meaning, anything that came be computed analytically, can be computed within this game.</p>
+            <p>Within the Game of Life, specific <a className="active-link" href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Examples_of_patterns" target="_blank" rel="noopener noreferrer">patterns</a> occur while be ran. There are common pattern types that have been recognized:</p>
+            <ul>
+                <li><b><i>Still lives</i></b> &rarr; patterns that do not change from one generation to the next</li>
+                <li><b><i>Oscillators</i></b> &rarr; patterns that return to their initial state after a certain number of generations</li>
+                <li><b><i>Spaceships</i></b> &rarr; patterns that move through out the grid</li>
+            </ul>
         </>
     )
 }
--- game-of-life/src/components/Rules.js
+++ game-of-life/src/components/Rules.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Rules() {
     return (
         <ol>
-            <li>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</li>
-            <li>Any live cell with two or three live neighbours lives on to the next generation.</li>
-            <li>Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
-            <li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+            <li>Any live cell with fewer than two live neighbors dies, as if by underpopulation.</li>
+            <li>Any live cell with two or three live neighbors lives on to the next generation.</li>
+            <li>Any live cell with more than three live neighbors dies, as if by overpopulation.</li>
+            <li>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
         </ol>
     )
 }
